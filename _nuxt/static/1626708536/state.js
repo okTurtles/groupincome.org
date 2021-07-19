80ccd71297d9e5dfc004c01c865d3af1d5e191a7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-19 15:29:07 +0000

    Deploy okTurtles/groupincome.org to github.com/okTurtles/groupincome.org.git:main

---
+++ _nuxt/static/1626708536/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgroupincome.org\u002F_nuxt\u002Fstatic\u002F1626708536",layout:"default",error:a,state:{openNav:false},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fgroupincome.org\u002F",assetsPath:"\u002Fgroupincome.org\u002F_nuxt\u002F",cdnURL:a}}}}(null));
