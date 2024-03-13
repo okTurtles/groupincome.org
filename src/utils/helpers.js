c256cd4a3c24712ba1181807467e3f2143e875e4
Sebin Song <songxevin@gmail.com>
2024-03-13 21:21:10 +1300

    #89 Add a new blog post, #92 Two footnotes related issues (#90)
    * add a blog post for 'the prototype is ready' page
    
    * add screenshot / figure out how to add footnotes
    
    * fix for the issue #92
    
    * add some blend-in effect to the header image
    
    * add comments for the footnote issue / add .has-blended-edge css class
    
    * better solution for footnote anchor tags
    
    * add box-shadow to blog images
    
    * add css-filter instead of box-shadow
    
    * gradient drop-shadow effect
    
    * little adjustment to the drop-shadow percentage
    
    * revert back to box-shadow from css-filter

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,9 @@
 'use strict'
 
-export function resolvePath (relPath) {
+export function resolvePath (relPath = '') {
   // used to resolve any assets or page index file paths with the baseUrl configured by 'base' option in astro.config.mjs
   // (reference: https://docs.astro.build/en/reference/configuration-reference/#base)
+  if (!relPath) { return undefined }
 
   relPath = relPath.startsWith('/') ? relPath.slice(1) : relPath
   return `${import.meta.env.BASE_URL}${relPath}`
