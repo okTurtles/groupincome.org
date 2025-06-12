659fced9292ccbde66bf029a3eee083afaeef860
Greg Slepak <contact@taoeffect.com>
2025-06-12 12:24:33 -0700

    fix social (Closes #170)

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,7 +35,7 @@ const { IS_PROD_BUILD } = process.env
 // https://astro.build/config
 export default defineConfig({
   site: IS_PROD_BUILD === 'true'
-    ? "https://our_prod_website_url.org" // !!NOTE!!: when the time comes and we deploy this project to our own website, replace this with the correct URL.
+    ? "https://groupincome.org" // !!NOTE!!: when the time comes and we deploy this project to our own website, replace this with the correct URL.
     : 'https://okturtles.github.io',
   base: IS_PROD_BUILD === 'true' ? "/" : '/groupincome.org',
   // integrations: [mdx(), sitemap(), vue()],
