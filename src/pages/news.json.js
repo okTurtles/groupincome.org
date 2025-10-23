b4dec2419dcbbbc805eb0eaf10fc8dab8d61a5bc
SebinSong <songxevin@gmail.com>
2025-10-23 14:57:34 +1300

    implement dynamic news.json endpoint generation

---
+++ src/pages/news.json.js
@@ -0,0 +1,17 @@
+export function GET () {
+  const newsPosts = Object.values(import.meta.glob('../posts/news/*.md', { eager: true }))
+  const newsJSONarray = newsPosts.map(post => {
+    return {
+      title: post.frontmatter.title,
+      createdAt: post.frontmatter.createdAt,
+      content: post.rawContent().trim()
+        .replace(/[ \t]+(?=\n)/g, '') // remove unnecessary whitespaces that occur between line-breaks (e.g. '\n     \n' -> '\n\n')
+    }
+  })
+  return new Response(
+    JSON.stringify(newsJSONarray),
+    {
+      headers: { 'Content-Type': 'application/json' }
+    }
+  )
+}
