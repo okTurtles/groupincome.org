8a05685bd703908b51aeab94b640e59381aaa055
SebinSong <songxevin@gmail.com>
2025-10-24 08:10:15 +1300

    add CORS related headers to the Reponse / add reference link

--- src/pages/news.json.js
+++ src/pages/news.json.js
@@ -2,6 +2,7 @@ const getPostDate = (post) => {
   return new Date(post.createdAt).getTime()
 }
 
+// reference: https://docs.astro.build/en/guides/endpoints/#static-file-endpoints
 export function GET () {
   const newsPosts = Object.values(import.meta.glob('../posts/news/*.md', { eager: true }))
   const newsJSONarray = newsPosts.map(post => {
@@ -9,15 +10,23 @@ export function GET () {
       title: post.frontmatter.title,
       createdAt: post.frontmatter.createdAt,
       content: post.rawContent().trim()
-        .replace(/[ \t]+(?=\n)/g, '') // remove unnecessary whitespaces that occur between line-breaks (e.g. '\n     \n' -> '\n\n')
+        // remove unnecessary whitespaces that occur between line-breaks (e.g. '\n     \n' -> '\n\n')
+        .replace(/[ \t]+(?=\n)/g, '')
     }
   })
 
+  // Sort the news posts by the latest first
   newsJSONarray.sort((a, b) => getPostDate(b) - getPostDate(a))
+
   return new Response(
     JSON.stringify(newsJSONarray),
     {
-      headers: { 'Content-Type': 'application/json' }
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*' // Enable CORS for the news.json endpoint
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      }
     }
   )
 }
