2ce1482bd7df0564f910aa99222f42c4c8159fc6
SebinSong <songxevin@gmail.com>
2025-12-17 14:48:35 +1300

    ensure no trailing whitespaces / comment for Astro.locals in the middleware definition

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { defineMiddleware } from "astro:middleware";
 // https://docs.astro.build/en/guides/middleware/
 export const onRequest = defineMiddleware((context, next) => {
   if (context.params.locale) {
+    // storing data in context.locals: https://docs.astro.build/en/guides/middleware/#storing-data-in-contextlocals
     context.locals.locale = context.params.locale
   }
   return next();
