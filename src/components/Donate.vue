15dae93dd228da70b2303dc032faa57cc1d7cb3d
SebinSong <songxevin@gmail.com>
2026-07-24 08:35:23 +0900

    some more feedback updates

--- src/components/Donate.vue
+++ src/components/Donate.vue
@@ -1,7 +1,7 @@
 <template>
   <ul>
     <li class="c-crypto-item" v-for="crypto in cryptos" @click="copyToClipBoard(crypto.id)" :key="crypto.id">
-      <label class="c-crypto-label">{{crypto.name}} ({{crypto.abreviation}})</label>
+      <label class="c-crypto-label">{{crypto.name}} ({{crypto.abbreviation}})</label>
 
       <div class="c-crypto-field">
         <span class="c-address-text">{{crypto.id}}</span>
@@ -22,19 +22,19 @@ export default {
       cryptos: [
         {
           name: 'Bitcoin',
-          abreviation: 'BTC',
+          abbreviation: 'BTC',
           id: '39ekronaSx3pBUuftNYfuwoWbGipnFrBv6'
         }, {
-          name: 'Etherium',
-          abreviation: 'ETH',
+          name: 'Ethereum',
+          abbreviation: 'ETH',
           id: '0x62992467ecd54d45fe5a028b4ccd082486ccc9a8'
         }, {
           name: 'Monero',
-          abreviation: 'XMR',
+          abbreviation: 'XMR',
           id: '899WAZkTAHv71bxeVu3coHcyCynEpbKWvhSfAxpRu41qXCfrMPz9cae7jzMS7mVAFb63mVxCm64NdVSFZ7Aey1nvE1Dgb7E'
         }, {
           name: 'Zcash',
-          abreviation: 'ZEC',
+          abbreviation: 'ZEC',
           id: 't1Vk6YJK4g7YYPPD8ENMoHBeNDqoQ67mCHx'
         }
       ],
